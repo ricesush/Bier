787542897649cbbdb11180e7be0982d72a59faad
ricesush <pixelartme@gmail.com>
2023-11-01 16:41:31 +0800

    Added navigation and 3 pages: about, contact and services

--- bier-ui/src/app/components/footer.jsx
+++ bier-ui/src/app/components/footer.jsx
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.css'
-
 export default function Footer() {
   return (
     <div className="bg-white">
---
+++ bier-ui/src/app/services/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <main>
+      <section className='container'>
+        This is the Services Page
+      </section>
+    </main>
+  )
+}
